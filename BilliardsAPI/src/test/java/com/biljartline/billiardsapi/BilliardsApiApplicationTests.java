9bbbbed9403ccb671144f0cf9d1fd94c6a1a9e31
Lars-v-Delft <lars171998@gmail.com>
2023-10-07 14:23:07 +0200

    BilliardsAPI setup
    (cherry picked from commit 409009adef9a8642b884ee34feac6a1c475aec3e)

---
+++ BilliardsAPI/src/test/java/com/biljartline/billiardsapi/BilliardsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biljartline.billiardsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilliardsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
