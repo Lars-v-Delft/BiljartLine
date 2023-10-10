057360c86604d20626f4b019cce75c189e27f6c9
Lars-v-Delft <lars171998@gmail.com>
2023-10-10 13:27:31 +0200

    Finished getcompetitionsbyfederationid

--- BilliardsAPI/src/main/java/com/biljartline/billiardsapi/competition/CompetitionDTO.java
+++ BilliardsAPI/src/main/java/com/biljartline/billiardsapi/competition/CompetitionDTO.java
@@ -1,19 +1,16 @@
 package com.biljartline.billiardsapi.competition;
 
-import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
 
-@Entity
 @Getter
-public class Competition {
-    @Id
+@Setter
+public class CompetitionDTO {
     private long id;
     private long federationId;
     private String name;
-    @Enumerated(EnumType.STRING)
     private GameType gameType;
-    private LocalDate endDate;
     private LocalDate startDate;
+    private LocalDate endDate;
 }
--- BilliardsAPI/src/main/java/com/biljartline/billiardsapi/competition/CompetitionService.java
+++ BilliardsAPI/src/main/java/com/biljartline/billiardsapi/competition/CompetitionService.java
@@ -1,4 +1,35 @@
 package com.biljartline.billiardsapi.competition;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class CompetitionService {
+    private final CompetitionRepo competitionRepo;
+    @Autowired
+    public CompetitionService(CompetitionRepo competitionRepo) {
+        this.competitionRepo = competitionRepo;
+    }
+
+    public List<CompetitionDTO> getByFederationId(long federationId){
+        List<Competition> temp = competitionRepo.findByFederationId(federationId);
+        return temp.stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+    }
+
+    private CompetitionDTO convertToDTO(Competition competition){
+        CompetitionDTO dto = new CompetitionDTO();
+        dto.setId(competition.getId());
+        dto.setFederationId(competition.getFederationId());
+        dto.setName(competition.getName());
+        dto.setGameType(competition.getGameType());
+        dto.setStartDate(competition.getStartDate());
+        dto.setEndDate(competition.getEndDate());
+        return dto;
+    }
 }
