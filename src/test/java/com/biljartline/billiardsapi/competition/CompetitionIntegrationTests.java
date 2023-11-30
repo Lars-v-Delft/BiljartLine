065e6f112382702a39b85a68a21f5cd63c7dd24e
Lars-v-Delft <lars171998@gmail.com>
2023-11-30 13:41:53 +0100

    added dev and test profiles, updated POM

--- src/test/java/com/biljartline/billiardsapi/competition/CompetitionIntegrationTests.java
+++ src/test/java/com/biljartline/billiardsapi/competition/CompetitionIntegrationTests.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -21,10 +21,10 @@ import java.util.Map;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest()
-@TestPropertySource(locations = "classpath:application-test.properties")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class CompetitionIntegrationTests {
 
     @Autowired
