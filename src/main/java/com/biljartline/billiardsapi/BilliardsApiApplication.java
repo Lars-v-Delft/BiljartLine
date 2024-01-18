b22cbcf6d67ad61d1e2beff32ebf07859b55d97d
Lars-v-Delft <lars171998@gmail.com>
2024-01-18 09:21:21 +0100

    allow all origins

--- src/main/java/com/biljartline/billiardsapi/BilliardsApiApplication.java
+++ src/main/java/com/biljartline/billiardsapi/BilliardsApiApplication.java
@@ -20,7 +20,7 @@ public class BilliardsApiApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("*")
                         .allowedMethods("*");
             }
         };
